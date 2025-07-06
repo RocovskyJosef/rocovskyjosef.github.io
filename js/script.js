0b80efb2fdfc502ca1b8dd7a0c1b80a52f73a6ac
Josef Ročovský <josefouncz@gmail.com>
2025-07-06 08:56:51 +0200

    first responsivity

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function menuOpen () {
+  var x = document.getElementById("mobile-menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
