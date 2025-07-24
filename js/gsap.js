65d91f5bc646cf9b8649e51bd35fb25f2c609c96
Josef Ročovský <josefouncz@gmail.com>
2025-07-24 08:18:05 +0200

    animated about section and added personal photo

--- js/gsap.js
+++ js/gsap.js
@@ -210,5 +210,70 @@ gsap.registerPlugin(Flip);
     });
 
 
+    let aboutMeH1 = SplitText.create("#about h1", {
+        type: "chars, lines",
+        autoSplit: true,
+    });
+
+    gsap.from(aboutMeH1.chars, {
+        scrollTrigger: {
+            trigger: "#about",
+            start: "center 70%",
+            end: "center 20%",
+            toggleActions: "play reverse play reverse",
+            //markers: true,
+        },
+        yPercent: "random([-30, 30])",
+        autoAlpha: 0,
+        stagger: {
+            amount: 1,
+            from: "random"
+        }
+    });
+
+        let aboutMeP = SplitText.create("#about p", {
+        type: "words, lines",
+        autoSplit: true,
+        mask: "lines",
+        smartSplit: true,
+
+        
+    });
+
+    gsap.from(aboutMeP.words, {
+        scrollTrigger: {
+            trigger: "#about",
+            start: "center 70%",
+            end: "center 20%",
+            toggleActions: "play reverse play reverse",
+            //markers: true,
+        },
+        y: 100,
+        autoAlpha: 0,
+        stagger: {
+            amount: 2,
+        },
+    });
+
+
+gsap.from("#about-image", {
+  scrollTrigger: {
+    trigger: "#about",
+    start: "center 70%",
+    end: "center 20%",
+    toggleActions: "play reverse play reverse",
+    // markers: true,
+  },
+  opacity: 0,
+  scale: 0.8,
+  duration: 1,
+});
+
+
+    
+
+
+
+
 
 });
